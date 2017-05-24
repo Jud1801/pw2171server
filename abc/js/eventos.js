16a9bb9f75b24b596cfe283372f48d60990ec75c
Jud1801 <judith_95geminis@hotmail.com>
2017-05-24 15:05:51 -0600

    Eventos.php-pendiente, datos.php valida en prueba

--- abc/js/eventos.js
+++ abc/js/eventos.js
@@ -1,6 +1,28 @@
 // $ = jquery - son equvalentes
 
 var iniciaApp = function(){
-	alert("Hola App :D");
+	// alert("Hola App :D");
+
+	//Funcionalidad
+	var entrar = function(){
+		alert($("#txtUsuario").val());
+		alert($("#txtClave").val());
+	}
+	var teclaUsuario = function(tecla){
+		if(tecla.which==13){
+			$("#txtClave").focus();
+		}
+	}
+	var teclaClave = function(tecla){
+		if(tecla.which==13){
+			entrar();
+		}
+	}
+
+	//Sección de declaración de eventos
+	$("#btnEntrar").on("click",entrar);
+	$("#txtUsuario").on("keypress", teclaUsuario);
+	$("#txtClave").on("keypress", teclaClave);
 }
-$(document).on("ready", iniciaApp);
+
+$(document).ready(iniciaApp);
