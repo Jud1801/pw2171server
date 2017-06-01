0db3c6ab9fd730488c44a85a600f1112fa236226
Jud1801 <judith_95geminis@hotmail.com>
2017-06-01 14:55:48 -0600

    Eventos, evento valida

--- abc/js/eventos.js
+++ abc/js/eventos.js
@@ -5,8 +5,37 @@ var iniciaApp = function(){
 
 	//Funcionalidad
 	var entrar = function(){
-		alert($("#txtUsuario").val());
-		alert($("#txtClave").val());
+		// alert($("#txtUsuario").val());
+		// alert($("#txtClave").val());
+		var usuario = $("#txtUsuario").val();
+		var clave 	= $("#txtClave").val();
+
+		var parametros = "opcion=valida"+
+						 "&usuario="+usuario+
+						 "&clave="+clave+
+						 "&id="+Math.random(); //El random sirve para que se este refrescando la información
+		
+		var validaEntrada = $.ajax({
+			method:"POST",
+			url:"php/datos.php",
+			data:parametros,
+			dataType:"json"
+		});
+
+		validaEntrada.done(function(data){
+			if(data.respuesta){
+				$("#datosUsuario").hide("slow");
+				$("nav").show("slow");
+			}
+			else{
+				alert("Usted no es bienvenido señor feo D:<");
+			}
+		});
+
+		validaEntrada.fail(function(jqError, textStatus){
+			alert("Salga corriendo D: "+textStatus);
+		});
+
 	}
 	var teclaUsuario = function(tecla){
 		if(tecla.which==13){
